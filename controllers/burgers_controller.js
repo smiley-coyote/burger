a916f864d9b1d597a77370a34ccce2b6926adecd
smiley-coyote <nick.a.alex@gmail.com>
2018-08-14 15:44:18 -0400

    added initial setups to files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/cat.js");
+
+// router code will go here
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,15 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+   all: function(){
+      // orm goes here
+   },
+   insert: function(){
+      // orm goes here
+   },
+   update: function(){
+      // orm goes here
+   }
+}
+
+module.exports = burger;
